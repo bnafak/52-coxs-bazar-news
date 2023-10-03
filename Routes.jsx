fd2a0fe129de341aa8627cb7c5da502f8af64990
Barun Nafak <bnafak@gmail.com>
2023-10-03 16:44:38 +0600

    first commit

---
+++ Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./src/layouts/Root";
+import Home from "./src/pages/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element:<Root></Root>,
+        children:[
+            {
+            path:'/',
+            element:<Home></Home>
+            }
+        ]
+    }
+]);
+export default router;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXLgtEv1qWs6AKzD26jabE-ClTn9mvxfI",
+  authDomain: "coxs-bazar-news.firebaseapp.com",
+  projectId: "coxs-bazar-news",
+  storageBucket: "coxs-bazar-news.appspot.com",
+  messagingSenderId: "349343149596",
+  appId: "1:349343149596:web:6c721ed1340cdf8a5639d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
